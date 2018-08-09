9c3a1e6ebfc56318520c1b810319771997132602
jsl028 <jsl028@bucknell.edu>
2018-08-08 22:46:06 -0400

    major upgrades

--- how-does-reddit-feel/server/server.js
+++ how-does-reddit-feel/server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 const bodyParser= require('body-parser')
-const COMMENT_SEARCH_URL = 'https://api.pushshift.io/reddit/comment/search?size=400&q=';
+const COMMENT_SEARCH_URL = 'https://api.pushshift.io/reddit/comment/search?size=400&fields=body&q=';
+const SUBMISSION_SEARCH_URL = 'https://api.pushshift.io/reddit/submission/search?size=400&q=';
+const TOP_SUBMISSIONS_URL = 'https://www.reddit.com/search.json?sort=top&type=link&t=day&q=';
 const port = process.env.PORT || 8080;
 //const SEARCH_URL = "https://www.reddit.com/search.json?q=Trump";
 
@@ -11,15 +13,32 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');next(); });
 app.set('view engine', 'ejs');
 
-app.get('/*', (req, res) => {
+app.get('/search/*', (req, res) => {
+  //console.log(req.query);
+  let search_query = COMMENT_SEARCH_URL + req.query.q.split(" ").join("%22");
+  if(req.query.type == "Posts") {
+    search_query = SUBMISSION_SEARCH_URL + req.query.q.split(" ").join("%22");
+  }
+  fetch(search_query)
+  .then(data => data.json())
+  .then(result => {
+    //console.log(data);
+
+    res.status(200).send(result);
+  }).catch(err => {
+    console.error(err);
+    res.status(500).send(renderHTML('Errar'));
+  });
+});
+
+app.get('/top/*', (req, res) => {
   console.log(req.query);
-  let query = COMMENT_SEARCH_URL + req.query.q.split(" ").join("%22");
-  //console.log(query);
+  let query = TOP_SUBMISSIONS_URL + req.query.q.split(" ").join("%22");
   fetch(query)
   .then(data => data.json())
   .then(data => {
     //console.log(data);
-    res.status(300).send(data);
+    res.status(300).send([data.data.children[0], data.data.children[1]]);
   }).catch(err => {
     console.error(err);
     res.status(500).send(renderHTML('Errar'));
--- how-does-reddit-feel/src/App.js
+++ how-does-reddit-feel/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import logo from './logo.png';
+import { Button, DropdownButton, FormGroup, InputGroup, MenuItem } from 'react-bootstrap'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Chart from "./react-google-charts";
 import InputHints from 'react-input-hints';
 import './App.css';
 
-const SEARCH_API = "https://boiling-stream-94503.herokuapp.com/?q=";
+//const SEARCH_API = "https://boiling-stream-94503.herokuapp.com/?q=";
+const SEARCH_API = "http://localhost:8080/search/?q=";
+const TOP_API = "http://localhost:8080/top/?q=";
 const vader = require('vader-sentiment');
 
 class App extends Component {
@@ -17,13 +18,21 @@ class App extends Component {
     super(props);
     this.state = {
       input: "",
-      loading: false,
-      loaded: false,
-      analysis: {}
+      loadingAnalysis: false,
+      loadingTrending: false,
+      loadingStarted: false,
+      analysis: {},
+      searchType: "Type",
+      trendingPost1: {},
+      trendingPost2: {}
     };
   }
 
-  _handleKeyPress = (e) => {
+  handleSearchType = (key) => {
+      this.setState({searchType: key});
+    }
+
+  handleKeyPress = (e) => {
       if (e.key === 'Enter') {
         this.handleSearch();
       }
@@ -31,9 +40,11 @@ class App extends Component {
 
   handleRefresh = () => {
     this.setState({ analysis: {} });
-    this.setState({ loading: false });
-    this.setState({ loaded: false });
+    this.setState({ loadingAnalysis: false });
+    this.setState({ loadingTrending: false });
+    this.setState({ loadingStarted: false });
     this.setState({ input: "" });;
+    this.setState({ searchType: "Type" });;
   }
 
   handleSearchInput = (e) => {
@@ -41,59 +52,97 @@ class App extends Component {
   }
 
   handleSearch = () => {
-    var that = this;
-    this.setState({loading: true, loadingMessage: "Loading comments..."})
-    var apiRequest = SEARCH_API + this.state.input.split(' ').join('%20');
-    fetch(apiRequest)
-    .then(function(response){
-      console.log('hit');
+  var that = this;
+  if (!this.state.input.length == 0) {
+    this.setState({loadingStarted: true, loadingMessage: "Loading comments...", loadingAnalysis: true, loadingTrending: true})
+    this.getTrendingPosts();
+    var apiRequest = SEARCH_API + this.state.input.split(' ').join('%20') + "&type=" + this.state.searchType;
+    fetch(apiRequest).then(function(response) {
       return response.json();
-    }).then(function(data){
+    }).then(function(data) {
       that.analyzeData(data.data);
+    })
+  }
+}
+
+  getTrendingPosts = () => {
+    var that = this;
+    var trendingRequest = TOP_API + this.state.input.split(' ').join('%20');
+    fetch(trendingRequest)
+    .then(function(response){
+      console.log(response);
+      return response.json();
+    }).then(function(results){
+      console.log(results);
+      try {
+        that.setState({trendingPost1: {
+          subredditLink: "http://www.reddit.com/" + results[0].data.subreddit_name_prefixed,
+          postLink: "http://www.reddit.com" + results[0].data.permalink,
+          title: results[0].data.title,
+          subreddit: results[0].data.subreddit_name_prefixed
+        }})
+
+        that.setState({trendingPost2: {
+          subredditLink: "http://www.reddit.com/" + results[1].data.subreddit_name_prefixed,
+          postLink: "http://www.reddit.com" + results[1].data.permalink,
+          title: results[1].data.title,
+          subreddit: results[1].data.subreddit_name_prefixed
+        }})
+      } catch (ex) {
+        console.log(ex);
+      }
+      that.setState({loadingTrending: false})
     })
   }
 
   analyzeData ( data ) {
-    console.log(data);
+    //console.log(data);
     this.setState({loadingMessage: "Analyzing sentiments..."})
     let count = 0;
     let pos_sum = 0;
     let neg_sum = 0;
-    let neu_sum = 0;
-    for(var i = 0; i < data.length; i++) {
-      if (data[i].body.length < 500) {
-        const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(data[i].body);
-        console.log(intensity);
-        pos_sum += intensity.pos;
-        neg_sum += intensity.neg;
-        neu_sum += intensity.neu;
-        count++;
+    if (this.state.searchType == "Comments" || this.state.searchType == "Type") {
+      for(var i = 0; i < data.length; i++) {
+        if (data[i].body.length < 500) {
+          const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(data[i].body);
+          pos_sum += intensity.pos;
+          neg_sum += intensity.neg;
+          count++;
+        }
       }
+    } else {
+      for(var i = 0; i < data.length; i++) {
+          const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(data[i].title);
+          pos_sum += intensity.pos;
+          neg_sum += intensity.neg;
+          count++;
     }
+  }
+
     this.setState({
       analysis: [["Sentiment", "Percentage"],
                   ["Positive", pos_sum/count],
                   ["Negative", neg_sum/count]
 
                 ],
-      loading: false,
-      loaded: true
+      loadingAnalysis: false
     });
   }
 
   render() {
+
+
     const options = {
       pieHole: 0.4,
       is3D: true
     };
 
-    if( this.state.loading ) {
+    if( this.state.loadingAnalysis || this.state.loadingTrending) {
       return (
         <div className="App">
         <header className="App-header">
         <div className = "headerNav" onClick={this.handleRefresh}>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> How Does Reddit Feel?</h1>
         </div>
         </header>
         <br/>
@@ -102,17 +151,19 @@ class App extends Component {
         {this.state.loadingMessage}
         </div>
       )
-    } else if ( this.state.loaded ) {
+    } else if ( this.state.loadingStarted ) {
       return (
         <div className="App">
         <header className="App-header">
         <div className = "headerNav" onClick={this.handleRefresh}>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> How Does Reddit Feel?</h1>
+
         </div>
         </header>
         <br/>
         <h2> Results for {this.state.input}</h2>
+        <div className="col-sm-6 mx-3">
+        <h3>Reddit's Sentiments</h3>
         <Chart
           chartType="PieChart"
           width="100%"
@@ -120,7 +171,27 @@ class App extends Component {
           data={this.state.analysis}
           options={options}
         />
-        <Button variant="contained" color="primary" onClick={this.handleRefresh}>Retry</Button>
+
+        </div>
+      <div className="col-sm-6 mx-3">
+      <h3>Trending Posts</h3>
+        <blockquote className="reddit-card" data-card-preview="0">
+          <a href={this.state.trendingPost1.postLink}>{this.state.trendingPost1.title}</a>
+          from
+          <a href={this.state.trendingPost1.subredditLink}>{this.state.trendingPost1.subreddit}</a>
+        </blockquote>
+        <blockquote className="reddit-card" data-card-preview="0">
+          <a href={this.state.trendingPost2.postLink}>{this.state.trendingPost2.title}</a>
+          from
+          <a href={this.state.trendingPost2.subredditLink}>{this.state.trendingPost2.subreddit}</a>
+        </blockquote>
+      </div>
+      <div className="col-sm-12">
+
+        <Button variant="contained" className="analyzeButton mb-5" onClick={this.handleRefresh}>Retry</Button>
+        <br/>
+        <br/>
+      </div>
 
         </div>)
       } else {
@@ -129,25 +200,39 @@ class App extends Component {
           <header className="App-header">
           <div className = "headerNav" onClick={this.handleRefresh}>
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title"> How Does Reddit Feel?</h1>
           </div>
           </header>
           <p className="App-intro">
           <br/>
-          To get started, enter a name you would like to analyze:
+          To get started, enter a topic you would like to analyze:
           <br/>
           </p>
-          Search: <InputHints className ="search" onKeyPress={this._handleKeyPress} onChange={this.handleSearchInput}
-          placeholders={[
-            'Donald Trump',
-            'A Quiet Place',
-            'Lebron James',
-            'Kim Jong Un'
-          ]}
-          />
+          <div className="col-sm-6 col-sm-offset-3">
+          <FormGroup>
+            <InputGroup>
+              <InputHints className="search" onKeyPress={this.handleKeyPress} onChange={this.handleSearchInput}
+              placeholders={[
+
+                'Bitcoin',
+                'A Quiet Place',
+                'Lebron James',
+                'Donald Trump'
+              ]}
+              />
+              <DropdownButton
+                componentClass={InputGroup.Button}
+                name="typeDropdown"
+                id="input-dropdown-addon"
+                title={this.state.searchType}
+                onSelect={this.handleSearchType}>
+                <MenuItem eventKey="Posts">Posts</MenuItem>
+                <MenuItem eventKey="Comments">Comments</MenuItem>
+              </DropdownButton>
+            </InputGroup>
+          </FormGroup>
           <br/>
-          <br/>
-          <Button variant="contained" color="primary" onClick={this.handleSearch}>Analyze</Button>
+          <Button variant="contained" className="analyzeButton" onClick={this.handleSearch}>Analyze</Button>
+          </div>
           </div>
         );
       }
